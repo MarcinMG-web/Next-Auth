de7f69aebf59cca9482d381455c4b1f7050a7686
MarcinMG-web <gierada.marcin@gmail.com>
2023-12-12 13:49:45 +0100

    added icon to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Image from 'next/image';
+import StainEffect from '../components/StainEffect/StainEffect';
+
 export default function Home() {
   return (
     <main className='flex flex-col items-center justify-between p-24'>
-      <h1>Home Page</h1>
+      <StainEffect />
+      <Image
+        className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
+        src='/next.svg'
+        alt='Next.js Logo'
+        width={200}
+        height={400}
+        priority
+      />
     </main>
   );
 }
